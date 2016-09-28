cb010c90540a0957b3dc6efc1ae21a82f4ad5727
crstn210 <crstn210@gmail.com>
2016-09-28 03:14:18 -0500

    add level to consulting firm contract, and implement bootstrap and bootstrap-datepicker

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,10 @@
 angular.module('Corporate-services').controller('MainCtrl', function($scope, $state, $timeout, $sessionStorage) {
     MainCtrl = this;
     $scope.$storage = $sessionStorage;
-    $timeout(function () {
-      $("#contractType").kendoDropDownList();
-      $("#sourceOfFounding").kendoDropDownList();
-      $("#typeOfAmendment").kendoDropDownList();
-      $("#calendar").kendoCalendar();
-      $("#calendar2").kendoCalendar();
-      $("#calendar3").kendoCalendar();
+    $timeout(function() {
+        $('.datepicker').datepicker({
+            format: 'd/MM/yyyy'
+        });
     }, 0);
 
 });
